67463af4307bc96a4df3b20238dcbb2910990ea6
muham <muhammadessamwork@outlook.com>
2023-10-09 14:07:12 +0300

    Adds table and all stuff

--- app/Console/Commands/MakeIsIteratableCommand.php
+++ app/Console/Commands/MakeIsIteratableCommand.php
@@ -15,19 +15,18 @@ class MakeIsIteratableCommand extends Command
     {
         \DB::statement('update evaluation_transactions set  is_iterated = 0;');
         $trans = EvaluationTransaction::all();
+        $dispatcher = EvaluationTransaction::getEventDispatcher();
+        EvaluationTransaction::unsetEventDispatcher();
         foreach ($trans as $tran) {
             $tran->timestamps = false;
             if (is_numeric($tran['instrument_number'])) {
                 $col = EvaluationTransaction::where('instrument_number', $tran['instrument_number'])->where('id', '!=', $tran['id']);
                 $col->timestamps = false;
-                $col->update([
-                    'is_iterated' => true
-                ]);
+                $col->update(['is_iterated' => true]);
             } else {
-                $tran->update([
-                    'is_iterated' => false
-                ]);
+                $tran->update(['is_iterated' => false]);
             }
         }
+        EvaluationTransaction::setEventDispatcher($dispatcher);
     }
 }
