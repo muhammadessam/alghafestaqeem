38be842d24cd9cc1dc74b2e739633d0aa93ddb10
muham <muhammadessamwork@outlook.com>
2023-10-11 16:00:22 +0300

    Adds table and all

--- app/Models/Evaluation/EvaluationTransaction.php
+++ app/Models/Evaluation/EvaluationTransaction.php
@@ -36,9 +36,10 @@ class EvaluationTransaction extends Model
 
     protected static function booted(): void
     {
-        static::created(function (EvaluationTransaction $evaluationTransaction) {
-            if (is_numeric($evaluationTransaction->instrument_number) and EvaluationTransaction::where('instrument_number', $evaluationTransaction->instrument_number)->where('id', '!=', $evaluationTransaction->id)->count()) {
+        static::creating(function (EvaluationTransaction $evaluationTransaction) {
+            if (is_numeric($evaluationTransaction->instrument_number) and EvaluationTransaction::where('instrument_number', $evaluationTransaction->instrument_number)->count()) {
                 \DB::update('update evaluation_transactions set is_iterated=1 where instrument_number=?', [$evaluationTransaction->instrument_number]);
+                $evaluationTransaction->is_iterated = true;
             }
         });
         static::updating(function (EvaluationTransaction $evaluationTransaction) {
