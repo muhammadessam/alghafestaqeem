29cabeda95fffe99a78176a7f4406ac6b17e2e38
Mosaab Emam <mosaab.emam123@gmail.com>
2024-01-19 12:27:47 +0000

    chore(contracts): :contruction: moving to an actual implementation

--- app/Http/Controllers/Admin/ContractController.php
+++ app/Http/Controllers/Admin/ContractController.php
@@ -16,161 +16,161 @@ class ContractController extends Controller
 
     public function create()
     {
-        $pdf = new \App\Helpers\MYPDF(PDF_PAGE_ORIENTATION, PDF_UNIT, PDF_PAGE_FORMAT, true, 'UTF-8', false);
+        // $pdf = new \App\Helpers\MYPDF(PDF_PAGE_ORIENTATION, PDF_UNIT, PDF_PAGE_FORMAT, true, 'UTF-8', false);
 
-        $pageCount = $pdf->setSourceFile(storage_path('pdf-templates/contract-template.pdf'));
-        $lg = array();
-        $lg['a_meta_charset'] = 'UTF-8';
-        $lg['a_meta_dir'] = 'rtl';
-        $lg['a_meta_language'] = 'fa';
-        $lg['w_page'] = 'page';
-        $pdf->setLanguageArray($lg);
-        $pdf->SetFont('aealarabiya', '', 12, false);
-        for ($pageNo = 1; $pageNo <= $pageCount; $pageNo++) {
-            $templateId = $pdf->importPage($pageNo);
-            $pdf->AddPage();
-            $pdf->useTemplate($templateId, ['adjustPageSize' => true]);
+        // $pageCount = $pdf->setSourceFile(storage_path('pdf-templates/contract-template.pdf'));
+        // $lg = array();
+        // $lg['a_meta_charset'] = 'UTF-8';
+        // $lg['a_meta_dir'] = 'rtl';
+        // $lg['a_meta_language'] = 'fa';
+        // $lg['w_page'] = 'page';
+        // $pdf->setLanguageArray($lg);
+        // $pdf->SetFont('aealarabiya', '', 12, false);
+        // for ($pageNo = 1; $pageNo <= $pageCount; $pageNo++) {
+        //     $templateId = $pdf->importPage($pageNo);
+        //     $pdf->AddPage();
+        //     $pdf->useTemplate($templateId, ['adjustPageSize' => true]);
 
-            if ($pageNo == 1) {
-                $pdf->setY(25);
-                $pdf->setFontSize(14);
-                $number_ln = 'عقد رقم: ' . '123456';
-                $pdf->Cell(0, 0, $number_ln, 0, 1, 'C', 0, '', 1);
+        //     if ($pageNo == 1) {
+        //         $pdf->setY(25);
+        //         $pdf->setFontSize(14);
+        //         $number_ln = 'عقد رقم: ' . '123456';
+        //         $pdf->Cell(0, 0, $number_ln, 0, 1, 'C', 0, '', 1);
 
-                $pdf->setY(45);
-                $pdf->setX(20);
-                $pdf->setFontSize(13);
-                $date_line = 'حرر هذا العقد بالرياض في '
-                    . ArabicDate::dayName()
-                    . ': '
-                    . ArabicDate::gregorianDate()
-                    . 'م'
-                    . ' الموافق: '
-                    . ArabicDate::hijriDate()
-                    . 'هـ'
-                    . ' بين كل من:';
-                $pdf->Cell(0, 0, $date_line, 0, 1, 'R', 0, '', 1);
+        //         $pdf->setY(45);
+        //         $pdf->setX(20);
+        //         $pdf->setFontSize(13);
+        //         $date_line = 'حرر هذا العقد بالرياض في '
+        //             . ArabicDate::dayName()
+        //             . ': '
+        //             . ArabicDate::gregorianDate()
+        //             . 'م'
+        //             . ' الموافق: '
+        //             . ArabicDate::hijriDate()
+        //             . 'هـ'
+        //             . ' بين كل من:';
+        //         $pdf->Cell(0, 0, $date_line, 0, 1, 'R', 0, '', 1);
 
-                $pdf->setFontSize(12);
-                $pdf->setY(154);
-                $pdf->setX(67);
-                $client_name = 'فلان الفلاني بن فلان العلاني';
-                $pdf->Cell(0, 0, $client_name, 0, 1, 'R', 0, '', 1);
+        //         $pdf->setFontSize(12);
+        //         $pdf->setY(154);
+        //         $pdf->setX(67);
+        //         $client_name = 'فلان الفلاني بن فلان العلاني';
+        //         $pdf->Cell(0, 0, $client_name, 0, 1, 'R', 0, '', 1);
 
-                $pdf->setY(162);
-                $pdf->setX(67);
-                $registration_number = '1234567890';
-                $pdf->Cell(0, 0, $registration_number, 0, 1, 'R', 0, '', 1);
+        //         $pdf->setY(162);
+        //         $pdf->setX(67);
+        //         $registration_number = '1234567890';
+        //         $pdf->Cell(0, 0, $registration_number, 0, 1, 'R', 0, '', 1);
 
-                $pdf->setY(170);
-                $pdf->setX(67);
-                $address = 'السودان';
-                $pdf->Cell(0, 0, $address, 0, 1, 'R', 0, '', 1);
+        //         $pdf->setY(170);
+        //         $pdf->setX(67);
+        //         $address = 'السودان';
+        //         $pdf->Cell(0, 0, $address, 0, 1, 'R', 0, '', 1);
 
-                $pdf->setY(178);
-                $pdf->setX(67);
-                $phone_numbers = '1234567890';
-                $pdf->Cell(0, 0, $phone_numbers, 0, 1, 'R', 0, '', 1);
+        //         $pdf->setY(178);
+        //         $pdf->setX(67);
+        //         $phone_numbers = '1234567890';
+        //         $pdf->Cell(0, 0, $phone_numbers, 0, 1, 'R', 0, '', 1);
 
-                $pdf->setY(186);
-                $pdf->setX(67);
-                $email = 'mosaab.emam123@gmail.com';
-                $pdf->Cell(0, 0, $email, 0, 1, 'R', 0, '', 1);
+        //         $pdf->setY(186);
+        //         $pdf->setX(67);
+        //         $email = 'mosaab.emam123@gmail.com';
+        //         $pdf->Cell(0, 0, $email, 0, 1, 'R', 0, '', 1);
 
-                $pdf->setY(194);
-                $pdf->setX(67);
-                $representative = 'Mosaab Emam';
-                $pdf->Cell(0, 0, $representative, 0, 1, 'R', 0, '', 1);
+        //         $pdf->setY(194);
+        //         $pdf->setX(67);
+        //         $representative = 'Mosaab Emam';
+        //         $pdf->Cell(0, 0, $representative, 0, 1, 'R', 0, '', 1);
 
-                $purpose = 'الــبــيــع';
-                $pdf->setY(230);
-                $pdf->setX(58);
-                $pdf->setFontSize(14);
-                $pdf->Cell(0, 0, $purpose, 0, 1, 'R', 0, '', 1);
-            }
-            if ($pageNo == 2) {
-                $purpose = 'البيع';
-                $purpose_ln = 'بناءً على طلب الطرف الثاني فان الغرض من معرفة القيمة السوقية للعقار محل التقييم هو (' . $purpose . ').';
-                $pdf->setY(186);
-                $pdf->setX(20);
-                $pdf->setFontSize(14);
-                $pdf->Cell(0, 0, $purpose_ln, 0, 1, 'R', 0, '', 1);
+        //         $purpose = 'الــبــيــع';
+        //         $pdf->setY(230);
+        //         $pdf->setX(58);
+        //         $pdf->setFontSize(14);
+        //         $pdf->Cell(0, 0, $purpose, 0, 1, 'R', 0, '', 1);
+        //     }
+        //     if ($pageNo == 2) {
+        //         $purpose = 'البيع';
+        //         $purpose_ln = 'بناءً على طلب الطرف الثاني فان الغرض من معرفة القيمة السوقية للعقار محل التقييم هو (' . $purpose . ').';
+        //         $pdf->setY(186);
+        //         $pdf->setX(20);
+        //         $pdf->setFontSize(14);
+        //         $pdf->Cell(0, 0, $purpose_ln, 0, 1, 'R', 0, '', 1);
 
-                $pdf->setY(212);
-                $pdf->setX(67);
-                $pdf->setFontSize(12);
-                $type = 'مجمع سكني';
-                $area = '1000';
-                $type_area_ln = 'العقار عبارة عن ' . $type . ' بمساحة: (' . $area . 'متر مربع)';
-                $pdf->Cell(0, 0, $type_area_ln, 0, 1, 'R', 0, '', 1);
+        //         $pdf->setY(212);
+        //         $pdf->setX(67);
+        //         $pdf->setFontSize(12);
+        //         $type = 'مجمع سكني';
+        //         $area = '1000';
+        //         $type_area_ln = 'العقار عبارة عن ' . $type . ' بمساحة: (' . $area . 'متر مربع)';
+        //         $pdf->Cell(0, 0, $type_area_ln, 0, 1, 'R', 0, '', 1);
 
-                $pdf->setY(220);
-                $pdf->setX(67);
-                $address = 'الحي الفلاني، شارع فلان بن فلان، جدة، السعودية';
-                $address_ln = 'يقع العقار بـ: ' . $address;
-                $pdf->Cell(0, 0, $address_ln, 0, 1, 'R', 0, '', 1);
+        //         $pdf->setY(220);
+        //         $pdf->setX(67);
+        //         $address = 'الحي الفلاني، شارع فلان بن فلان، جدة، السعودية';
+        //         $address_ln = 'يقع العقار بـ: ' . $address;
+        //         $pdf->Cell(0, 0, $address_ln, 0, 1, 'R', 0, '', 1);
 
-                $pdf->setY(228);
-                $pdf->setX(67);
-                $registration_number = '1234567890';
-                $registration_number_ln = 'رقم (' . $registration_number . ') بتاريخ: ' . date('Y/m/d');
-                $pdf->Cell(0, 0, $registration_number_ln, 0, 1, 'R', 0, '', 1);
-            }
-            if ($pageNo == 3) {
-                $pdf->setY(35);
-                $pdf->setX(100);
-                $no_of_assets = '1';
-                $pdf->Cell(0, 0, $no_of_assets, 0, 1, 'R', 0, '', 1);
+        //         $pdf->setY(228);
+        //         $pdf->setX(67);
+        //         $registration_number = '1234567890';
+        //         $registration_number_ln = 'رقم (' . $registration_number . ') بتاريخ: ' . date('Y/m/d');
+        //         $pdf->Cell(0, 0, $registration_number_ln, 0, 1, 'R', 0, '', 1);
+        //     }
+        //     if ($pageNo == 3) {
+        //         $pdf->setY(35);
+        //         $pdf->setX(100);
+        //         $no_of_assets = '1';
+        //         $pdf->Cell(0, 0, $no_of_assets, 0, 1, 'R', 0, '', 1);
 
-                $pdf->setY(35);
-                $pdf->setX(125);
-                $taqeem_cost = '10,000';
-                $pdf->Cell(0, 0, $taqeem_cost, 0, 1, 'R', 0, '', 1);
+        //         $pdf->setY(35);
+        //         $pdf->setX(125);
+        //         $taqeem_cost = '10,000';
+        //         $pdf->Cell(0, 0, $taqeem_cost, 0, 1, 'R', 0, '', 1);
 
-                $pdf->setY(35);
-                $pdf->setX(165);
-                $total_cost = '10,000';
-                $pdf->Cell(0, 0, $total_cost, 0, 1, 'R', 0, '', 1);
+        //         $pdf->setY(35);
+        //         $pdf->setX(165);
+        //         $total_cost = '10,000';
+        //         $pdf->Cell(0, 0, $total_cost, 0, 1, 'R', 0, '', 1);
 
-                $pdf->setY(43);
-                $pdf->setX(165);
-                $tax = '1,500';
-                $pdf->Cell(0, 0, $tax, 0, 1, 'R', 0, '', 1);
+        //         $pdf->setY(43);
+        //         $pdf->setX(165);
+        //         $tax = '1,500';
+        //         $pdf->Cell(0, 0, $tax, 0, 1, 'R', 0, '', 1);
 
-                $pdf->setY(51);
-                $pdf->setX(165);
-                $total_in_numbers = '11,500';
-                $pdf->Cell(0, 0, $total_in_numbers, 0, 1, 'R', 0, '', 1);
+        //         $pdf->setY(51);
+        //         $pdf->setX(165);
+        //         $total_in_numbers = '11,500';
+        //         $pdf->Cell(0, 0, $total_in_numbers, 0, 1, 'R', 0, '', 1);
 
-                $pdf->setY(59);
-                $pdf->setX(125);
-                $total_in_words = 'إحدى عشر الف وخمسمائة ريال فقط';
-                $pdf->Cell(0, 0, $total_in_words, 0, 1, 'R', 0, '', 1);
-            }
-            if ($pageNo == 4) {
-                $pdf->setY(241);
-                $pdf->setX(124);
-                $client_name = 'فلان الفلاني بن فلان العلاني';
-                $pdf->Cell(0, 0, $client_name, 0, 1, 'R', 0, '', 1);
+        //         $pdf->setY(59);
+        //         $pdf->setX(125);
+        //         $total_in_words = 'إحدى عشر الف وخمسمائة ريال فقط';
+        //         $pdf->Cell(0, 0, $total_in_words, 0, 1, 'R', 0, '', 1);
+        //     }
+        //     if ($pageNo == 4) {
+        //         $pdf->setY(241);
+        //         $pdf->setX(124);
+        //         $client_name = 'فلان الفلاني بن فلان العلاني';
+        //         $pdf->Cell(0, 0, $client_name, 0, 1, 'R', 0, '', 1);
 
-                $dataPieces = explode(',', Contract::first()->signature);
-                $encodedImg = $dataPieces[1];
-                $decodedImg = base64_decode($encodedImg);
-                //  Check if image was properly decoded
-                if ($decodedImg !== false) {
-                    if (file_put_contents('new-test.png', $decodedImg) !== false) {
-                        $pdf->Image('new-test.png', 80, 248, 16, 16, 'png');
-                        unlink('new-test.png');
-                    }
-                }
+        //         $dataPieces = explode(',', Contract::first()->signature);
+        //         $encodedImg = $dataPieces[1];
+        //         $decodedImg = base64_decode($encodedImg);
+        //         //  Check if image was properly decoded
+        //         if ($decodedImg !== false) {
+        //             if (file_put_contents('new-test.png', $decodedImg) !== false) {
+        //                 $pdf->Image('new-test.png', 80, 248, 16, 16, 'png');
+        //                 unlink('new-test.png');
+        //             }
+        //         }
 
-                $pdf->setY(266);
-                $pdf->setX(124);
-                $pdf->Cell(0, 0, date('Y/m/d'), 0, 1, 'R', 0, '', 1);
-            }
-        }
-        $pdf->Output(public_path('test' . now()->toDateString() . '.pdf'), 'F');
-        return response()->download(public_path('test' . now()->toDateString() . '.pdf'))->deleteFileAfterSend(true);
+        //         $pdf->setY(266);
+        //         $pdf->setX(124);
+        //         $pdf->Cell(0, 0, date('Y/m/d'), 0, 1, 'R', 0, '', 1);
+        //     }
+        // }
+        // $pdf->Output(public_path('test' . now()->toDateString() . '.pdf'), 'F');
+        // return response()->download(public_path('test' . now()->toDateString() . '.pdf'))->deleteFileAfterSend(true);
         return view('admin.contracts.create');
     }
 
