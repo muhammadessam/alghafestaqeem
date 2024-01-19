033cc44598f6fe9d2b7db93c087fefe0e6ead9bb
Mosaab Emam <mosaab.emam123@gmail.com>
2024-01-19 16:11:01 +0000

    chore(contracts): push a new build

--- vite.config.js
+++ vite.config.js
@@ -1,27 +1,28 @@
-import laravel, { refreshPaths } from 'laravel-vite-plugin'
-import { defineConfig } from 'vite'
+import laravel, { refreshPaths } from "laravel-vite-plugin";
+import { defineConfig } from "vite";
 
 export default defineConfig({
     server: {
-        host: 'localhost',
+        host: "localhost",
     },
     plugins: [
         laravel({
             input: [
-                'resources/css/signature-pad.css',
-                'resources/css/app.css',
-                'resources/js/signature_pad.umd.js',
-                'resources/js/app.js'
+                "resources/css/signature-pad.css",
+                "resources/css/app.css",
+                "resources/js/signature_pad.umd.js",
+                "resources/js/sign.js",
+                "resources/js/app.js",
             ],
             refresh: [
                 ...refreshPaths,
-                'app/Filament/**',
-                'app/Forms/Components/**',
-                'app/Livewire/**',
-                'app/Infolists/Components/**',
-                'app/Providers/Filament/**',
-                'app/Tables/Columns/**',
+                "app/Filament/**",
+                "app/Forms/Components/**",
+                "app/Livewire/**",
+                "app/Infolists/Components/**",
+                "app/Providers/Filament/**",
+                "app/Tables/Columns/**",
             ],
         }),
     ],
-})
+});
