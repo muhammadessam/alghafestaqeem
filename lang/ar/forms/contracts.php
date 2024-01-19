1c1c02344dec9c0b8ba7e73a2affecaed3b61d86
Mosaab Emam <mosaab.emam123@gmail.com>
2024-01-19 12:26:48 +0000

    feat(contracts): i18n and add missing translation keys

---
+++ lang/ar/forms/contracts.php
@@ -0,0 +1,24 @@
+<?php
+
+return [
+  'client_name' => 'اسم العميل',
+  'id_number' => 'رقم الهوية/السجل',
+  'client_address' => 'عنوان العميل',
+  'phone_numbers' => 'أرقام التواصل',
+  'phone_numbers_helper_text' => 'قم بفصل الأرقام باستخدام علامة ,',
+  'email' => 'البريد الالكتروني',
+  'representative_name' => 'اسم الممثل لها في العقد',
+  'purpose' => 'الغرض من التقييم',
+  'type' => 'نوع الأصل',
+  'area' => 'المساحة',
+  'area_suffix' => 'متر مربع',
+  'property_address' => 'عنوان الأصل',
+  'deed_number' => 'رقم صك الملكية',
+  'deed_issue_date' => 'تاريخ صك الملكية',
+  'number_of_assets' => 'عدد الأصول',
+  'cost_per_asset' => 'أتعاب الأصل الواحد',
+  'total_cost' => 'مجموع الأتعاب',
+  'tax' => 'الضريبة',
+  'total_cost_in_words' => 'مجموع الأتعاب كتابة',
+  'submit' => 'إنشاء العقد'
+];
---
+++ lang/en/forms/contracts.php
@@ -0,0 +1,24 @@
+<?php
+
+return [
+  'client_name' => 'Client Name',
+  'id_number' => 'ID Number',
+  'client_address' => 'Client Address',
+  'phone_numbers' => 'Contact Numbers',
+  'phone_numbers_helper_text' => 'Separate Multiple Entries Using Commas',
+  'email' => 'Email',
+  'representative_name' => 'Representative Name',
+  'purpose' => 'Purpose of the Review',
+  'type' => 'Type of property',
+  'area' => 'Area',
+  'area_suffix' => 'Square meters',
+  'property_address' => 'Property Address',
+  'deed_number' => 'Deed Number',
+  'deed_issue_date' => 'Deed Issue Date',
+  'number_of_assets' => 'Number of Assets',
+  'cost_per_asset' => 'Cost Per Asset',
+  'total_cost' => 'Total Cost',
+  'tax' => 'Tax',
+  'total_cost_in_words' => 'Total Cost in Words',
+  'submit' => 'Create Contract'
+];
