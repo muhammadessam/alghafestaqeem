d2b93e606974b4e4f22abf8e065a91d65c588a17
Mosaab Emam <mosaab.emam123@gmail.com>
2024-01-21 13:06:17 +0000

    feat(blog): blog is reachable from header

--- config/zeus-sky.php
+++ config/zeus-sky.php
@@ -6,7 +6,7 @@ return [
     /**
      * set the default path for the blog homepage.
      */
-    'prefix' => 'sky',
+    'prefix' => 'blog',
 
     /**
      * the middleware you want to apply on all the blog routes
