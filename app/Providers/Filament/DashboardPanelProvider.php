e3aa79dac391598cdb611d77818ed76c00736a79
Mosaab Emam <mosaab.emam123@gmail.com>
2024-01-21 07:41:17 +0000

    feat(cms): register sky as a plugin

--- app/Providers/Filament/DashboardPanelProvider.php
+++ app/Providers/Filament/DashboardPanelProvider.php
@@ -8,6 +8,7 @@ use Filament\Http\Middleware\DispatchServingFilamentEvent;
 use Filament\Pages;
 use Filament\Panel;
 use Filament\PanelProvider;
+use Filament\SpatieLaravelTranslatablePlugin;
 use Filament\Support\Colors\Color;
 use Filament\Widgets;
 use Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse;
@@ -17,6 +18,7 @@ use Illuminate\Routing\Middleware\SubstituteBindings;
 use Illuminate\Session\Middleware\AuthenticateSession;
 use Illuminate\Session\Middleware\StartSession;
 use Illuminate\View\Middleware\ShareErrorsFromSession;
+use LaraZeus\Sky\SkyPlugin;
 
 class DashboardPanelProvider extends PanelProvider
 {
@@ -53,6 +55,10 @@ class DashboardPanelProvider extends PanelProvider
             ])
             ->authMiddleware([
                 Authenticate::class,
+            ])
+            ->plugins([
+                SpatieLaravelTranslatablePlugin::make()->defaultLocales([config('app.locale')]),
+                SkyPlugin::make(),
             ]);
     }
 }
