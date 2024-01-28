b45dca8aa17870acf857840de3c89896133aeeef
Mosaab Emam <mosaab.emam123@gmail.com>
2024-01-28 08:09:29 +0000

    feat(et): register time notifications by scheduler

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -2,6 +2,10 @@
 
 namespace App\Console;
 
+use App\Models\Evaluation\EvaluationTransaction;
+use App\Models\User;
+use App\Notifications\TimeNotification;
+use Carbon\Carbon;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
@@ -12,16 +16,32 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule): void
     {
-  $schedule->command('backup:clean')->daily()->at('01:00');
-       $schedule->command('backup:run')->daily()->at('01:30');   
-}
+        $schedule->command('backup:clean')->daily()->at('01:00');
+        $schedule->command('backup:run')->daily()->at('01:30');
+
+        // Send notifications for appointments
+        $schedule->call(function () {
+            $user = User::find('1');
+            $start_dt = Carbon::now()->subDays(3)->format('Y-m-d H:m:s');
+            $end_dt = Carbon::now()->addDays(3)->format('Y-m-d H:m:s');
+            foreach (['preview', 'income', 'review'] as $type) {
+                $evaluation_transactions = EvaluationTransaction::where('status', 3)
+                    ->whereDate($type . '_date_time', '>', $start_dt)
+                    ->whereDate($type . '_date_time', '<', $end_dt)
+                    ->get();
+                foreach ($evaluation_transactions as $evaluation_transaction) {
+                    $user->notify(new TimeNotification($type, $evaluation_transaction));
+                }
+            }
+        })->everyMinute();
+    }
 
     /**
      * Register the commands for the application.
      */
     protected function commands(): void
     {
-        $this->load(__DIR__.'/Commands');
+        $this->load(__DIR__ . '/Commands');
 
         require base_path('routes/console.php');
     }
